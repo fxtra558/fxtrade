472da45b357184ff5d3f7e4df914177859a15171
fxtra558 <fxtra558@gmail.com>
2026-01-20 14:49:57 -0500

    Update data.py

--- data.py
+++ data.py
@@ -3,12 +3,8 @@ import pandas as pd
 
 class DataProvider:
     def get_ohlc(self, symbol="EURUSD=X", interval="1h"):
-        # symbol for EURUSD in Yahoo Finance is 'EURUSD=X'
         data = yf.download(tickers=symbol, period="5d", interval=interval, progress=False)
-        if data.empty:
-            return None
-        
+        if data.empty: return None
         df = data.copy()
-        # Clean column names
         df.columns = [col[0].lower() if isinstance(col, tuple) else col.lower() for col in df.columns]
         return df
